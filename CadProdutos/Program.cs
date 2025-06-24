e3ff4749cf415ac8b69e5d516c5045ca28d30f89
aldealexandre <alde.alexandre@hotmail.com>
2025-06-24 12:13:58 -0300

    Primeiros end points

--- CadProdutos/Program.cs
+++ CadProdutos/Program.cs
@@ -1,3 +1,5 @@
+using System.Windows.Markup;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,29 +18,30 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+
+var produtos = new List<Produto>()
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    new Produto() {Id = 1, Nome = "Teclado", Preco = 23.20M, Estoque = 24},
+    new Produto() {Id = 2, Nome = "Monitor", Preco = 230.50M, Estoque = 12}
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/produtos", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    return produtos;
+});
+
+app.MapGet("/produtos/{id}", (int id) =>
+{
+    var produto = produtos.FirstOrDefault(x => x.Id == id);
+    return produto is not null ? Results.Ok(produto) : Results.NotFound($"Produto com ID {id} não encontrado.");
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+class Produto
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public decimal Preco { get; set; }
+    public int Estoque { get; set; }
 }
